0e3d23a6b12b405037412271ef5a8de59270bd7c
Edmund Neil <edmondneil@gmail.com>
2024-07-21 21:13:51 -0600

    Update pushover.py

--- pushover.py
+++ pushover.py
@@ -1,9 +1,11 @@
 import http.client, urllib
+token = os.environ ['TOKEN']
+user = os.environ ['USER']
 conn = http.client.HTTPSConnection("api.pushover.net:443")
 conn.request("POST", "/1/messages.json",
   urllib.parse.urlencode({
-    "token": "ao4owdr5mze34jks1pt3iryrn26z5a",
-    "user": "ucz4hngocjn2zub71upf89ir3ho7z8",
+    "token": token,
+    "user": user,
     "message": "hello world",
   }), { "Content-type": "application/x-www-form-urlencoded" })
 conn.getresponse()
